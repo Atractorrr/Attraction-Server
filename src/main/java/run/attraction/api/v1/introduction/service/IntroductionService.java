73ca461105344a3f3c725aa649ae2ae7eb7e84de
ryan-dia <cjfdnjs158@naver.com>
2024-05-17 01:21:22 +0900

    [ATR-64] feat: 이전 아티클 get api 구현

--- src/main/java/run/attraction/api/v1/introduction/service/IntroductionService.java
+++ src/main/java/run/attraction/api/v1/introduction/service/IntroductionService.java
@@ -1,10 +1,14 @@
 package run.attraction.api.v1.introduction.service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import run.attraction.api.v1.introduction.Article;
 import run.attraction.api.v1.introduction.Newsletter;
 import run.attraction.api.v1.introduction.dto.response.NewsletterResponse;
+import run.attraction.api.v1.introduction.dto.response.PreviousArticleResponse;
 import run.attraction.api.v1.introduction.exception.ErrorMessages;
 import run.attraction.api.v1.introduction.repository.NewsletterRepository;
 
@@ -17,7 +21,19 @@ public class IntroductionService {
   public NewsletterResponse getNewsletter(Long newsletterId) {
     Newsletter newsletter = newsletterRepository.findById(newsletterId)
         .orElseThrow(() -> new NoSuchElementException(ErrorMessages.NOT_EXIST_NEWSLETTER.getViewName()));
+
     return new NewsletterResponse(newsletter);
+  }
+
+  public List<PreviousArticleResponse> getPreviousArticles(Long newsletterId, int size) {
+    Newsletter newsletter = newsletterRepository.findById(newsletterId)
+        .orElseThrow(() -> new NoSuchElementException(ErrorMessages.NOT_EXIST_NEWSLETTER.getViewName()));
+    List<Article> previousArticles = newsletter.getArticles();
+
+    return previousArticles.stream()
+        .limit(size)
+        .map(PreviousArticleResponse::new)
+        .collect(Collectors.toList());
   }
 }
 
