92374bc76e2cdce82c342cd9fcdc41af666f86e5
ryan-dia <cjfdnjs158@naver.com>
2024-05-17 20:15:59 +0900

    [ATR-66] feat: 연관된 뉴스레터 get api 구현

--- src/main/java/run/attraction/api/v1/introduction/service/IntroductionService.java
+++ src/main/java/run/attraction/api/v1/introduction/service/IntroductionService.java
@@ -2,12 +2,15 @@ package run.attraction.api.v1.introduction.service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Random;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import run.attraction.api.v1.introduction.Article;
+import run.attraction.api.v1.introduction.Category;
 import run.attraction.api.v1.introduction.Newsletter;
 import run.attraction.api.v1.introduction.dto.response.NewsletterResponse;
+import run.attraction.api.v1.introduction.dto.response.NewslettersByCategoryResponse;
 import run.attraction.api.v1.introduction.dto.response.PreviousArticleResponse;
 import run.attraction.api.v1.introduction.exception.ErrorMessages;
 import run.attraction.api.v1.introduction.repository.NewsletterRepository;
@@ -33,6 +36,39 @@ public class IntroductionService {
     return previousArticles.stream()
         .limit(size)
         .map(PreviousArticleResponse::new)
+        .collect(Collectors.toList());
+  }
+
+  public List<NewslettersByCategoryResponse> getRelatedNewslettersByCategory(Long newsletterId, int size) {
+    Newsletter newsletter = newsletterRepository.findById(newsletterId)
+        .orElseThrow(() -> new NoSuchElementException(ErrorMessages.NOT_EXIST_NEWSLETTER.getViewName()));
+    Category category = newsletter.getCategory();
+    final int count = newsletterRepository.countByCategoryAndIdNot(category, newsletterId);
+
+    if (count <= size) {
+      return getNewslettersByCategory(newsletterId, category, size);
+    }
+    return getRandomNewslettersByCategory(count, size, category, newsletterId);
+
+
+  }
+
+  private List<NewslettersByCategoryResponse> getNewslettersByCategory(Long newsletterId, Category category, int size) {
+    List<Newsletter> newsletters = newsletterRepository.findByCategoryAndIdNotWithOffset(category.name(), newsletterId, size, 0);
+
+    return newsletters.stream()
+        .map(NewslettersByCategoryResponse::new)
+        .collect(Collectors.toList());
+  }
+
+  private List<NewslettersByCategoryResponse> getRandomNewslettersByCategory(int count, int size, Category category, Long newsletterId ) {
+    final int maxOffset = count - size;
+    final int offset = new Random().nextInt(maxOffset + 1);
+
+    List<Newsletter> newsletters = newsletterRepository.findByCategoryAndIdNotWithOffset(category.name(), newsletterId, size, offset);
+
+    return newsletters.stream()
+        .map(NewslettersByCategoryResponse::new)
         .collect(Collectors.toList());
   }
 }
