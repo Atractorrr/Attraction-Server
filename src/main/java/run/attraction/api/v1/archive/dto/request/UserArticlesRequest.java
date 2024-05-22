533ba3950697f4abf142a438f457a3d860d7745a
ryan-dia <cjfdnjs158@naver.com>
2024-05-22 18:28:05 +0900

    [ATR-69] feat: userArticlesRequest 추가

---
+++ src/main/java/run/attraction/api/v1/archive/dto/request/UserArticlesRequest.java
@@ -0,0 +1,19 @@
+package run.attraction.api.v1.archive.dto.request;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserArticlesRequest {
+  private  Long userId;
+  private  int page = 0;
+  private  int size = 20;
+  private  String[] sort = {"receivedAt","asc"};
+  private  String isHideRead = "false";
+  private  String category;
+  private  String q; // search query
+}
