2adb9d1f3847ddd413e320181c732e80c416a5b1
ryan-dia <cjfdnjs158@naver.com>
2024-05-23 10:32:16 +0900

    [ATR-106] feat: entity 수정
    - 멘토링에의한 entity 수정
    - 뉴스레터 entity 수정
    - 아티클 entity 수정

--- src/main/java/run/attraction/api/v1/archive/Article.java
+++ src/main/java/run/attraction/api/v1/archive/Article.java
@@ -1,11 +1,11 @@
-package run.attraction.api.v1.introduction;
+package run.attraction.api.v1.archive;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,33 +23,33 @@ public class Article {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
-  private Newsletter newsletter;
+  @Column(nullable = false, length = 100)
+  private String newsletterEmail;
+
+  @Column(nullable = false, length = 100)
+  private String userEmail;
 
   @Column(nullable = false, length = 100)
   private String title;
 
   @Column(nullable = false)
-  private String thumbnail;
+  private String thumbnailUrl;
 
   @Column(nullable = false)
   private String contentUrl;
 
   @Column(nullable = false)
-  private String content;
+  private int readingTime;
 
   @Column(nullable = false)
-  private int readingTime;
+  private String contentSummary;
+
+  @Column(nullable = false)
+  boolean isDeleted = false;
 
   @Column(nullable = false)
   private Date receivedAt;
 
-  public Article(Long id, String title, String thumbnail, String content, String contentUrl, int readingTime) {
-    this.id = id;
-    this.title = title;
-    this.thumbnail = thumbnail;
-    this.content = content;
-    this.contentUrl = contentUrl;
-    this.readingTime = readingTime;
-  }
+  @Column(nullable = false)
+  LocalDate createAt;
 }
