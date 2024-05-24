06f60047f036dbea4f03f560b7a57dd8db8f6392
ryan-dia <cjfdnjs158@naver.com>
2024-05-24 21:41:47 +0900

    [ATR-128] - updateUserArticlePercentage 구현 -

--- src/main/java/run/attraction/api/v1/archive/controller/ArchiveController.java
+++ src/main/java/run/attraction/api/v1/archive/controller/ArchiveController.java
@@ -3,31 +3,38 @@ package run.attraction.api.v1.archive.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import run.attraction.api.v1.archive.dto.response.ApiResponse;
 import run.attraction.api.v1.archive.dto.ArticleDTO;
 import run.attraction.api.v1.archive.dto.request.UserArticlesRequest;
+import run.attraction.api.v1.archive.dto.response.ApiResponse;
 import run.attraction.api.v1.archive.service.ArchiveService;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
 public class ArchiveController {
 
   private final ArchiveService archiveService;
 
-  @GetMapping("/api/v1/user/{userEmail}/articles")
-  public ResponseEntity<ApiResponse<Page<ArticleDTO>>> getUserArticles(@PathVariable String userEmail, UserArticlesRequest request) {
-
+  @GetMapping("/{userEmail}/articles")
+  public ApiResponse<Page<ArticleDTO>> getUserArticles(@PathVariable String userEmail, UserArticlesRequest request) {
     request.setUserEmail(userEmail);
     Page<ArticleDTO> articles = archiveService.findArticlesByUserId(request.getUserEmail(), request);
-    ApiResponse<Page<ArticleDTO>> response = ApiResponse.from(HttpStatus.OK, "성공", articles);
 
-    return ResponseEntity.ok(response);
+    return ApiResponse.from(HttpStatus.OK, "성공", articles);
+  }
+
+  @PutMapping("/{userEmail}/article/{articleId}")
+  public ApiResponse<Void> updateUserArticlePercentage(@PathVariable String userEmail, @PathVariable Long articleId, @RequestParam int percentage) {
+    archiveService.updateUserArticlePercentage(userEmail, articleId, percentage);
+
+    return ApiResponse.from(HttpStatus.OK, "성공", null);
   }
 }
--- src/main/java/run/attraction/api/v1/archive/repository/ReadBoxRepository.java
+++ src/main/java/run/attraction/api/v1/archive/repository/ReadBoxRepository.java
@@ -1,6 +1,7 @@
 package run.attraction.api.v1.archive.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import run.attraction.api.v1.archive.ReadBox;
@@ -8,4 +9,5 @@ import run.attraction.api.v1.archive.ReadBox;
 public interface ReadBoxRepository extends JpaRepository<ReadBox, Long> {
   @Query("SELECT rb FROM ReadBox rb WHERE rb.userEmail = :userEmail AND rb.percentage = 100")
   List<ReadBox> findCompletedReadBoxByEmail(String userEmail);
+  Optional<ReadBox> findByUserEmailAndArticleId(String userEmail, Long articleId);
 }
