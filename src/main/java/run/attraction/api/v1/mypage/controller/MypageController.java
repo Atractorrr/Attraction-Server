ff0cc50d229232dcb1035501d9ba79765203702a
kim0527 <joo14753@naver.com>
2024-05-26 02:42:41 +0900

    [ATR-92] feat: 최근 읽은 아티클 API (Controller)

--- src/main/java/run/attraction/api/v1/mypage/controller/MypageController.java
+++ src/main/java/run/attraction/api/v1/mypage/controller/MypageController.java
@@ -1,5 +1,6 @@
 package run.attraction.api.v1.mypage.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import run.attraction.api.v1.mypage.service.MypageService;
+import run.attraction.api.v1.mypage.service.dto.archive.article.MypageArticle;
+import run.attraction.api.v1.mypage.service.dto.archive.article.RecentArticlesResponseDto;
 import run.attraction.api.v1.mypage.service.dto.calendar.CalendarResponseDto;
 import run.attraction.api.v1.mypage.service.dto.userDetail.UserDetailDto;
 import run.attraction.api.v1.mypage.service.dto.userDetail.UserDetailsResponseDto;
@@ -32,4 +35,9 @@ public class MypageController {
     return ResponseEntity.ok(userCalendar);
   }
 
+  @GetMapping("/{email}/articles/recent")
+  public final ResponseEntity<?> getRecentArticles(@PathVariable("email") String email) {
+    final List<MypageArticle> recentArticlesByEmail = mypageService.getRecentArticlesByEmail(email);
+    return ResponseEntity.ok(new RecentArticlesResponseDto(recentArticlesByEmail));
+  }
 }
