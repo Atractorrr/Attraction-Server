5ca610db5ff5de969b8f136df497117b63437f5c
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2024-05-27 11:47:03 +0900

    [ATR-167] feat: 유저가 뉴스레터 구독하면 구독함에 추가 (#38)
    - addNewsletter 메서드 추가
    - subscribe entity 추가
    - subscribe repository 추가

--- src/main/java/run/attraction/api/v1/archive/service/ArchiveService.java
+++ src/main/java/run/attraction/api/v1/archive/service/ArchiveService.java
@@ -1,6 +1,5 @@
 package run.attraction.api.v1.archive.service;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,10 +8,15 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import run.attraction.api.v1.archive.ReadBox;
+import run.attraction.api.v1.archive.Subscribe;
 import run.attraction.api.v1.archive.dto.ArticleDTO;
 import run.attraction.api.v1.archive.dto.request.UserArticlesRequest;
 import run.attraction.api.v1.archive.repository.ArticleRepository;
 import run.attraction.api.v1.archive.repository.ReadBoxRepository;
+import run.attraction.api.v1.archive.repository.SubscribeRepository;
+import run.attraction.api.v1.introduction.Newsletter;
+import run.attraction.api.v1.introduction.exception.ErrorMessages;
+import run.attraction.api.v1.introduction.repository.NewsletterRepository;
 
 
 @Service
@@ -21,6 +25,8 @@ public class ArchiveService {
 
   final private ArticleRepository articleRepository;
   final private ReadBoxRepository readBoxRepository;
+  final private NewsletterRepository newsletterRepository;
+  final private SubscribeRepository subscribeRepository;
 
   @Transactional(readOnly = true)
   public Page<ArticleDTO> findArticlesByUserId(String userEmail, UserArticlesRequest request) {
@@ -39,11 +45,27 @@ public class ArchiveService {
   public void saveUserArticleProgress(String userEmail, Long articleId, int percentage) {
     ReadBox readBox = readBoxRepository.findByUserEmailAndArticleId(userEmail, articleId)
         .orElse(createReadBox(userEmail, articleId));
+
     readBox.updatePercentage(percentage);
     readBoxRepository.save(readBox);
   }
 
   private ReadBox createReadBox(String userEmail, Long articleId) {
     return new ReadBox(userEmail, articleId);
+  }
+
+  @Transactional
+  public void addNewsletter(String userEmail, Long newsletterId) {
+    Newsletter newsletter = newsletterRepository.findById(newsletterId)
+        .orElseThrow(() -> new IllegalArgumentException(ErrorMessages.NOT_EXIST_NEWSLETTER.getViewName()));
+    Subscribe subscribe = subscribeRepository.findByUserEmail(userEmail)
+        .orElse(createSubscribe(userEmail));
+
+    subscribe.saveNewsletter(newsletter);
+    subscribeRepository.save(subscribe);
+  }
+
+  private Subscribe createSubscribe(String userEmail) {
+    return new Subscribe(userEmail);
   }
 }
