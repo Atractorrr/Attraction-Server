14fbbbd363d266e947a4b617cdd3885c4fb26bd8
uhanuu <hwryu990927@gmail.com>
2024-05-30 02:12:03 +0900

    [ATR-195] chore: import 제거

--- src/main/java/run/attraction/api/v1/gmail/GmailApiAccessException.java
+++ src/main/java/run/attraction/api/v1/gmail/GmailApiAccessException.java
@@ -1,7 +1,5 @@
 package run.attraction.api.v1.gmail;
 
-import java.io.IOException;
-
 public class GmailApiAccessException extends RuntimeException {
   public GmailApiAccessException(Throwable cause) {
     super(cause);
--- src/main/java/run/attraction/api/v1/gmail/GoogleClientSecretsNotLoadedException.java
+++ src/main/java/run/attraction/api/v1/gmail/GoogleClientSecretsNotLoadedException.java
@@ -1,7 +1,5 @@
 package run.attraction.api.v1.gmail;
 
-import java.io.IOException;
-
 public class GoogleClientSecretsNotLoadedException extends RuntimeException {
   public GoogleClientSecretsNotLoadedException(Throwable cause) {
     super(cause);
