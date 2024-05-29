ae4fe71a543ddb2b217a19470218a05e12d0d1ff
uhanuu <hwryu990927@gmail.com>
2024-05-30 02:08:14 +0900

    [ATR-195] feat: gmail에서 filter + label 처리

---
+++ src/main/java/run/attraction/api/v1/gmail/GmailApiAccessException.java
@@ -0,0 +1,9 @@
+package run.attraction.api.v1.gmail;
+
+import java.io.IOException;
+
+public class GmailApiAccessException extends RuntimeException {
+  public GmailApiAccessException(Throwable cause) {
+    super(cause);
+  }
+}
