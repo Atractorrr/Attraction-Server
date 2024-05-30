b8ca80c44104bb4cefaa56aa4d6a2a059d0049a8
H_JU_0527 <143387515+kim0527@users.noreply.github.com>
2024-05-30 18:16:17 +0900

    배포 테스트 6 (#64)
    * style: log 추가
    
    * style: log 작업

--- src/main/java/run/attraction/api/v1/user/service/UserServiceImpl.java
+++ src/main/java/run/attraction/api/v1/user/service/UserServiceImpl.java
@@ -3,10 +3,12 @@ package run.attraction.api.v1.user.service;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import run.attraction.api.v1.user.User;
 import run.attraction.api.v1.user.repository.UserRepository;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -18,8 +20,11 @@ public class UserServiceImpl implements UserService {
   }
 
   public void updateUserExpiration(User user, LocalDate now){
+    log.info("updateUserExpiration 진입");
     final long betweenDays = ChronoUnit.DAYS.between(user.getUpdateAt(), now);
+    log.info("renewUpdateAtAndExpirationAt 시작");
     user.renewUpdateAtAndExpirationAt(now,user.getUserExpiration().plusDays(betweenDays));
+    log.info("save 시작");
     userRepository.save(user);
   }
 }
