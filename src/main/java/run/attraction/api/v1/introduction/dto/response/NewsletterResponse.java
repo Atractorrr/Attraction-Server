7c57041699c59341b543c67d0b6a6d3808485193
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2024-06-01 01:11:28 +0900

    [ATR-204] remove: Uploadays enum 삭제 (#75)
    * remove: UploadDays enum 삭제
    
    * feat: uploadDays 타입변경
    
    - List<UploadDays> -> String으로 변경
    
    * chore: uploadDays enum 삭제로 인하여 더미 데이터 변경

--- src/main/java/run/attraction/api/v1/introduction/dto/response/NewsletterResponse.java
+++ src/main/java/run/attraction/api/v1/introduction/dto/response/NewsletterResponse.java
@@ -1,14 +1,12 @@
 package run.attraction.api.v1.introduction.dto.response;
 
-import java.util.List;
 import run.attraction.api.v1.introduction.Category;
 import run.attraction.api.v1.introduction.Newsletter;
-import run.attraction.api.v1.introduction.UploadDays;
 
 public record NewsletterResponse(
     String name,
     String description,
-    List<UploadDays> uploadDays,
+    String uploadDays,
     Category category,
     String mainLink,
     String subscribeLink,
