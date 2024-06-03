9dd765e15996b04beefb7d52deaf719834080ed7
ryan-dia <cjfdnjs158@naver.com>
2024-06-04 03:05:09 +0900

    [ATR-246] feat: 에러메시지 추가
    존재 하지 않는 아티클입니다

--- src/main/java/run/attraction/api/v1/introduction/exception/ErrorMessages.java
+++ src/main/java/run/attraction/api/v1/introduction/exception/ErrorMessages.java
@@ -11,7 +11,8 @@ public enum ErrorMessages {
   NOT_EXIST_DATA("존재 하지 않는 데이터입니다"),
   NOT_EXIST_NEWSLETTER("존재하지 않는 뉴스레터입니다"),
   REQUEST_PARAMETER_MISSING("필수 요청 파라미터가 누락되었습니다: "),
-  NOT_EXIST_NEWSLETTER_BY_CATEGORY("카테고리에 맞는 뉴스레터가 존재하지않습니다");
+  NOT_EXIST_NEWSLETTER_BY_CATEGORY("카테고리에 맞는 뉴스레터가 존재하지않습니다"),
+  NOT_EXIST_ARTICLE("존재 하지 않는 아티클입니다");
 
   private final String viewName;
 }
