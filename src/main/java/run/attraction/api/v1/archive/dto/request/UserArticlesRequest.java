c574dba40f46d80efb560546dc1a0af4fcb6062d
ryan-dia <cjfdnjs158@naver.com>
2024-06-06 09:21:34 +0900

    [ATR-260] fix: 최신순 오래된순 정렬이 서로 반대되는 현상 수정

--- src/main/java/run/attraction/api/v1/archive/dto/request/UserArticlesRequest.java
+++ src/main/java/run/attraction/api/v1/archive/dto/request/UserArticlesRequest.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 public class UserArticlesRequest {
   private int page = 0;
   private int size = 20;
-  private String[] sort = {"receivedAt", "asc"};
+  private String[] sort = {"receivedAt", "desc"};
   private String isHideRead = "false";
   private String category;
   private String q; // search query
