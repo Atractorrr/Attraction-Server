632d7e7931cfcc881b81867df42442685ce89b67
kim0527 <joo14753@naver.com>
2024-06-13 22:24:18 +0900

    [ATR-298] feat: OccupationStatistics 계산기
    - NewsletterEventRepository의 데이터로 조회
    - 직업별로 가장 인기 있는 뉴스레터를 하나씩 계산

--- src/main/java/run/attraction/api/v1/statistics/repository/NewsletterEventRepository.java
+++ src/main/java/run/attraction/api/v1/statistics/repository/NewsletterEventRepository.java
@@ -1,7 +1,9 @@
 package run.attraction.api.v1.statistics.repository;
 
 import java.util.List;
+import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import run.attraction.api.v1.statistics.AgeGroup;
 import run.attraction.api.v1.statistics.NewsletterEvent;
 import run.attraction.api.v1.user.Occupation;
@@ -9,4 +11,15 @@ import run.attraction.api.v1.user.Occupation;
 public interface NewsletterEventRepository extends JpaRepository<NewsletterEvent,Long> {
   List<NewsletterEvent> findByOccupation(Occupation occupation);
   List<NewsletterEvent> findByAgeGroup(AgeGroup ageGroup);
+
+  @Query(value = """
+          SELECT occupation, newsletter_id 
+          FROM (SELECT occupation, newsletter_id, 
+                       ROW_NUMBER() OVER (PARTITION BY occupation ORDER BY COUNT(newsletter_id) DESC) as rn 
+                FROM statistics_newsletter_event
+                WHERE created_at < :endDate
+                GROUP BY occupation, newsletter_id) subquery 
+          WHERE rn = 1
+       """,nativeQuery = true)
+  List<Object[]> findMostPopularNewsletterOccupationPairs(LocalDateTime endDate);
 }
