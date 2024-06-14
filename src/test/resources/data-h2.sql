fbaae0fa85aa633eeb8110e09d2c33434a0b313e
H_JU_0527 <143387515+kim0527@users.noreply.github.com>
2024-06-14 17:04:19 +0900

    [ATR-271] feat: 닉네임 Validator 수정 및 마이페이지 테스트 코드 추가 (#142)
    * [ATR-274] feat: 닉네임 대소문자 구분 추가 (#120)
    
    - 문자열 컬럼에서 대소문자를 구분하기 위해 binary 옵션 추가
    
    * [ATR-268] test: 마이페이지 테스트 코드 추가 (#123)

---
+++ src/test/resources/data-h2.sql
@@ -0,0 +1,56 @@
+-- 테이블이 이미 존재하는 경우 삭제
+DROP TABLE IF EXISTS newsletter_ids;
+DROP TABLE IF EXISTS subscribe;
+DROP TABLE IF EXISTS interests;
+DROP TABLE IF EXISTS newsletter;
+
+CREATE TABLE newsletter_ids (
+    subscribe_id BIGINT,
+    newsletter_id BIGINT,
+    PRIMARY KEY (subscribe_id, newsletter_id)
+);
+
+
+CREATE TABLE interests (
+    email VARCHAR(255) NOT NULL,
+    interests VARCHAR(255) NOT NULL,
+    PRIMARY KEY (email, interests)
+);
+
+CREATE TABLE IF NOT EXISTS newsletter (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    category VARCHAR(255),
+    created_at TIMESTAMP,
+    description VARCHAR(255),
+    email VARCHAR(255),
+    is_deleted BOOLEAN,
+    main_link VARCHAR(255),
+    modified_at TIMESTAMP,
+    name VARCHAR(255),
+    nickname VARCHAR(255),
+    subscribe_link VARCHAR(255),
+    thumbnail_url VARCHAR(255),
+    upload_days VARCHAR(255)
+    );
+
+INSERT INTO `interests` (email, interests) VALUES
+    ('test1@gmail.com', 'LIVING_INTERIOR'),
+    ('test1@gmail.com', 'FOOD'),
+    ('test1@gmail.com', 'LOCAL_TRAVEL'),
+    ('test1@gmail.com', 'HOBBY_SELF_DEVELOPMENT'),
+    ('test2@gmail.com', 'LIVING_INTERIOR'),
+    ('test2@gmail.com', 'IT_TECH'),
+    ('test2@gmail.com', 'BUSINESS_FINANCIAL_TECHNOLOGY'),
+    ('test2@gmail.com', 'CURRENT_AFFAIRS_SOCIETY'),
+    ('test3@gmail.com', 'DESIGN'),
+    ('test3@gmail.com', 'TREND_LIFE'),
+    ('test3@gmail.com', 'LOCAL_TRAVEL');
+
+INSERT INTO `newsletter_ids` (subscribe_id, newsletter_id) VALUES
+    (2, 6),
+    (2, 18),
+    (2, 44),
+    (2, 50),
+    (2, 62),
+    (3, 28),
+    (3, 31);
