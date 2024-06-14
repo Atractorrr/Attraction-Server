26342bbd17b930817e6c38fcf3d93422786457b8
ryan-dia <cjfdnjs158@naver.com>
2024-06-14 10:26:02 +0900

    feat: 북마크 기능 구현 추가
    - 북마크 추가
    - 북마크 삭제
    - 북마크 여부 확인
    - 북마크한 아티클 가져오기

--- src/main/java/run/attraction/api/v1/archive/repository/ArticleRepositoryCustom.java
+++ src/main/java/run/attraction/api/v1/archive/repository/ArticleRepositoryCustom.java
@@ -1,6 +1,7 @@
 package run.attraction.api.v1.archive.repository;
 
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,5 +9,6 @@ import run.attraction.api.v1.archive.dto.ArticleDTO;
 
 public interface ArticleRepositoryCustom {
   Page<ArticleDTO> findArticlesByUserEmail(String userEmail, String category, Boolean isRead, String search,  Pageable pageable);
+  Page<ArticleDTO> findArticlesByArticleIds(List<Long> articleIds, String category, String search, Pageable pageable);
   Optional<ArticleDTO> findArticleByUserEmailAndArticleId(String userEmail, Long articleId);
 }
