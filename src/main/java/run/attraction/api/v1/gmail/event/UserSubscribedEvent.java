2ff4608963dd32f0513377afadcc7039fb35bfd3
uhanuu <hwryu990927@gmail.com>
2024-06-20 21:42:41 +0900

    refactor: spring kafka 변경 및 비동기 처리

--- src/main/java/run/attraction/api/v1/gmail/event/UserSubscribedEvent.java
+++ src/main/java/run/attraction/api/v1/gmail/event/UserSubscribedEvent.java
@@ -3,11 +3,13 @@ package run.attraction.api.v1.gmail.event;
 import io.jsonwebtoken.lang.Assert;
 
 public record UserSubscribedEvent(
+    String userEmail,
     String newsletterEmail,
     String token
 ) {
   public UserSubscribedEvent {
+    Assert.hasText(userEmail, "사용자 이메일이 존재하지 않습니다.");
     Assert.hasText(newsletterEmail, "뉴스레터 이메일이 존재하지 않습니다.");
-    Assert.hasText(token, newsletterEmail +"을 가진 사용자의 Google 토큰이 존재하지 않습니다.");
+    Assert.hasText(token, newsletterEmail + "을 가진 사용자의 Google 토큰이 존재하지 않습니다.");
   }
 }
