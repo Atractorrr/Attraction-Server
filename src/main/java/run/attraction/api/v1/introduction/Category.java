100389f9c5b22d8fa053a4a67962e10a787a3319
ryan-dia <cjfdnjs158@naver.com>
2024-06-21 16:10:23 +0900

    style: 오타 수정
    엔터테이먼트 -> 엔터테인먼트

--- src/main/java/run/attraction/api/v1/introduction/Category.java
+++ src/main/java/run/attraction/api/v1/introduction/Category.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Category {
   TREND_LIFE("트렌드/라이프"),
-  ENTERTAINMENT("엔터테이먼트"),
+  ENTERTAINMENT("엔터테인먼트"),
   BUSINESS_FINANCIAL_TECHNOLOGY("비즈/재테크"),
   LOCAL_TRAVEL("지역/여행"),
   FOOD("푸드"),
@@ -17,7 +17,8 @@ public enum Category {
   HOBBY_SELF_DEVELOPMENT("취미/자기개발"),
   CULTURE_ART("문화/예술"),
   LIVING_INTERIOR("리빙/인테리어"),
-  ECONOMY("경제");
+  ECONOMY("경제"),
+  ;
 
   private final String viewName;
 }
