fe701f9ce43fc1d8da7244699ef53b1d66dc9fb9
kim0527 <joo14753@naver.com>
2024-06-24 21:42:01 +0900

    [ATR-432] feat: 세션 로그인 적용

--- src/main/java/run/attraction/api/v1/auth/service/dto/UserStateDto.java
+++ src/main/java/run/attraction/api/v1/auth/service/dto/UserStateDto.java
@@ -4,24 +4,16 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class UserTokenDto {
-  private String accessToken;
-  private String refreshToken;
+public class UserStateDto {
   private String email;
   private boolean isUserBefore;
   private boolean shouldReissueToken;
   private boolean hasExtraDetails;
 
   @Builder
-  private UserTokenDto(String accessToken, String refreshToken, String email, boolean isUserBefore,boolean hasExtraDetails) {
-    this.accessToken = accessToken;
-    this.refreshToken = refreshToken;
+  private UserStateDto(String email, boolean isUserBefore) {
     this.email = email;
     this.isUserBefore = isUserBefore;
-  }
-
-  public boolean getShouldReissueToken() {
-    return shouldReissueToken;
   }
 
   public void setShouldReissueToken(boolean shouldReissueToken) {
