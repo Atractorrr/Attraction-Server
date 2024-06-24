868d782f911360db76b5d0c703a77341689b5d1d
kim0527 <joo14753@naver.com>
2024-06-24 21:41:15 +0900

    [ATR-432] chore: 불필요한 jwt 관련 로직 삭제

--- src/main/java/run/attraction/api/v1/auth/token/exception/TokenExpirationException.java
+++
@@ -1,7 +0,0 @@
-package run.attraction.api.v1.auth.token.exception;
-
-public class TokenExpirationException extends RuntimeException {
-  public TokenExpirationException(String message) {
-    super(message);
-  }
-}
