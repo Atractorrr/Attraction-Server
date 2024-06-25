d9cd32a39d901e812b701a88a35a15227d7ea111
H_JU_0527 <143387515+kim0527@users.noreply.github.com>
2024-06-25 22:59:02 +0900

    [ATR-271] feat: 유저 관심사 수정 및 세션 ID 유저 정보 조회 API 구현 (#200)
    * [ATR-458] feat: 유저 관심사 변경서항 적용
    
    - '건강/의학' 제거
    - '경제' 추가
    
    * [ATR-461] feat: 세션 ID로 유저 정보 조회 api 구현
    
    * [ATR-461] feat: 세션 예외 추가

--- src/main/java/run/attraction/api/v1/auth/session/SessionService.java
+++ src/main/java/run/attraction/api/v1/auth/session/SessionService.java
@@ -2,18 +2,25 @@ package run.attraction.api.v1.auth.session;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import run.attraction.api.v1.auth.service.dto.UserStateDto;
+import run.attraction.api.v1.auth.session.dto.UserDetailBySession;
+import run.attraction.api.v1.auth.session.exception.SessionIdNotFoundException;
+import run.attraction.api.v1.user.repository.UserRepository;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class SessionService {
   public static final String LOGIN_MEMBER = "LOGIN_MEMBER";
 
+  private final UserRepository userRepository;
+
   public void getSession(HttpServletRequest request, UserStateDto userStateDto){
     HttpSession session = request.getSession();
-    session.setAttribute("LOGIN_MEMBER",userStateDto.getEmail());
+    session.setAttribute(LOGIN_MEMBER,userStateDto.getEmail());
   }
 
   public void removeSession(HttpServletRequest request){
@@ -21,5 +28,14 @@ public class SessionService {
     if (session != null) {
       session.invalidate();
     }
+  }
+
+  public UserDetailBySession getUserDetail(HttpServletRequest request){
+    HttpSession session = request.getSession(false);
+    if (session == null){
+      throw new SessionIdNotFoundException();
+    }
+    String email = (String)session.getAttribute(LOGIN_MEMBER);
+    return userRepository.getUserDetailsByEmail(email);
   }
 }
