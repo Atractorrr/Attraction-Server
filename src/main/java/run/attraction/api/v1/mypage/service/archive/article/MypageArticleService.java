2da648d0ffd287765a293209516dbab26c4dcfbd
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2024-06-30 04:28:05 +0900

    [ATR-510] fix: 구독하지 않은 뉴스레터의 아티클이 보이는 현상 수정 (#232)
    - 구독하지 않은 뉴스레터의 아티클은 표기하지 않도록 변경
    - 유저 구독리스트에서 구독하고 있는 뉴스레터 참고하도록 로직 추가
    - 너무 많은 데이터가 나가지 않도록 size 추가 (default size = 10)

--- src/main/java/run/attraction/api/v1/mypage/service/archive/article/MypageArticleService.java
+++ src/main/java/run/attraction/api/v1/mypage/service/archive/article/MypageArticleService.java
@@ -4,5 +4,5 @@ import java.util.List;
 import run.attraction.api.v1.mypage.service.dto.archive.article.RecentArticlesDto;
 
 public interface MypageArticleService {
-  List<RecentArticlesDto> getUserRecentArticles(String email);
+  List<RecentArticlesDto> getUserRecentArticles(String email, int size);
 }
