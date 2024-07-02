7c9b1545a1a7e62f975ab73258f4fd466f5838ac
H_JU_0527 <143387515+kim0527@users.noreply.github.com>
2024-07-02 13:42:26 +0900

    [ATR-522] feat: 세션으로 유저 정보 조회 API 수정 (#243)
    - 유저의 role 추가

--- src/main/java/run/attraction/api/v1/auth/session/dto/UserDetailBySession.java
+++ src/main/java/run/attraction/api/v1/auth/session/dto/UserDetailBySession.java
@@ -1,11 +1,13 @@
 package run.attraction.api.v1.auth.session.dto;
 
 import java.util.Objects;
+import run.attraction.api.v1.user.Role;
 
 public record UserDetailBySession(
     String email,
     String nickname,
     String profileImg,
+    Role role,
     boolean hasExtraDetails,
     boolean shouldReissueToken
 ) {
