ef97b2ac7e110ad0654b1484bd25200a41c6749e
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2024-07-04 03:09:30 +0900

    [ATR-536] feat: 마이페이지 뉴스레터 구독함 로직 수정 (#251)
    - (이전)해당 뉴스레터가 없다면 에러 발생으로 중단
    - (수정)해당 뉴스레터가 없다면 pass

--- src/main/java/run/attraction/api/v1/mypage/service/archive/newsletter/MypageNewsletterServiceImpl.java
+++ src/main/java/run/attraction/api/v1/mypage/service/archive/newsletter/MypageNewsletterServiceImpl.java
@@ -1,10 +1,12 @@
 package run.attraction.api.v1.mypage.service.archive.newsletter;
 
+
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import run.attraction.api.v1.introduction.Newsletter;
 import run.attraction.api.v1.introduction.Subscription;
-import run.attraction.api.v1.introduction.exception.ErrorMessages;
 import run.attraction.api.v1.introduction.repository.SubscriptionRepository;
 import run.attraction.api.v1.introduction.repository.NewsletterRepository;
 import run.attraction.api.v1.mypage.service.dto.archive.newsletter.MypageNewsletterDetail;
@@ -23,13 +25,16 @@ public class MypageNewsletterServiceImpl implements MypageNewsletterService {
       return List.of();
     }
 
-    return subscriptions
-        .stream()
-        .map(subscription -> newsletterRepository.findById(subscription.getNewsletterId())
-            .map(newsletter -> new MypageNewsletterDetail(
-                newsletter.getId(),
-                newsletter.getThumbnailUrl(),
-                newsletter.getName()))
-            .orElseThrow(() -> new IllegalArgumentException(ErrorMessages.NOT_EXIST_NEWSLETTER.getViewName()))).toList();
+    return subscriptions.stream()
+        .map(subscription -> newsletterRepository.findById(subscription.getNewsletterId()))
+        .filter(Optional::isPresent)
+        .map(optionalNewsletter -> {
+          Newsletter newsletter = optionalNewsletter.get();
+          return new MypageNewsletterDetail(
+              newsletter.getId(),
+              newsletter.getThumbnailUrl(),
+              newsletter.getName());
+        })
+        .toList();
   }
 }
