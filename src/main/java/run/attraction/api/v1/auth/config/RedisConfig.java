fce380535336f84bcc8cf29f048c73e5c61bfe93
H_JU_0527 <143387515+kim0527@users.noreply.github.com>
2024-08-06 13:30:59 +0900

    [ATR-595] feat: 세션 Redis 적용 (#287)

--- src/main/java/run/attraction/api/v1/auth/config/RedisConfig.java
+++ src/main/java/run/attraction/api/v1/auth/config/RedisConfig.java
@@ -1,11 +1,21 @@
 package run.attraction.api.v1.auth.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableRedisRepositories
 @Configuration
@@ -20,6 +30,32 @@ public class RedisConfig {
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
     return new LettuceConnectionFactory(host, port);
+  }
+
+  @Bean
+  public RedisTemplate<String,Object> redisTemplate() {
+    final RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(RedisSerializer.java());
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    return redisTemplate;
+  }
+
+  private ObjectMapper objectMapper() {
+    PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator
+        .builder()
+        .allowIfSubType(Object.class)
+        .build();
+
+    return new ObjectMapper()
+        .findAndRegisterModules()
+        .enable(SerializationFeature.INDENT_OUTPUT)
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false)
+        .registerModule(new JavaTimeModule())
+        .activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
   }
 }
 
