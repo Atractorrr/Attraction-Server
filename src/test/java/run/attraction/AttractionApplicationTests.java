fd0b2bffd5ac5f7f9607be22f2507696cdd9a8d7
kim0527 <joo14753@naver.com>
2024-10-01 10:44:54 +0900

    chore: 테스트코드에서 Redis 비활성화

--- src/test/java/run/attraction/AttractionApplicationTests.java
+++ src/test/java/run/attraction/AttractionApplicationTests.java
@@ -1,12 +1,12 @@
 package run.attraction;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = { RedisAutoConfiguration.class })
+@ImportAutoConfiguration(exclude = {RedisAutoConfiguration.class})
 class AttractionApplicationTests {
 
 	@Test
